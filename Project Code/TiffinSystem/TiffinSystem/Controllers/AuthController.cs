fa58d5c7da9a87f65ffa48af14ad161bf359b931
Kiran Vidhate <kiran.vidhate@aress.com>
2019-08-14 07:27:33 +0530

    Files added

---
+++ Project Code/TiffinSystem/TiffinSystem/Controllers/AuthController.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using TiffinSystem.Models;
+
+namespace TiffinSystem.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignIn(UserDetail userDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var context = new DBContext())
+                {
+                    if (context.UserDetails.Any(x => x.Email == userDetail.Email && x.Password == userDetail.Password))
+                    {
+                        UserDetail user = context.UserDetails.Single(x => x.Email == userDetail.Email);
+                        if (user.UserTypeId == 1)
+                        {
+                            Session["UserProfile"] = user.Email;
+                            return RedirectToAction("Index", "Admin");
+                        }
+                        else if (user.UserTypeId == 2)
+                        {
+                            Session["UserProfile"] = user.Email;
+                            return RedirectToAction("Index", "Vendor");
+                        }
+                        else
+                        {
+                            Session["UserProfile"] = user.Email;
+                            return RedirectToAction("Index", "User");
+                        }
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Invalid username and password");
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(UserDetail userDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                userDetail.UserTypeId = 3;
+                userDetail.Date = System.DateTime.Now;
+                using (var context = new DBContext())
+                {
+                    context.UserDetails.Add(userDetail);
+                    context.SaveChanges();
+                    return RedirectToAction("SignIn");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Project Code/TiffinSystem/TiffinSystem/Controllers/VendorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using TiffinSystem.Models;
+using System.Data.Entity;
+using System.Linq;
+using System;
+
+namespace TiffinSystem.Controllers
+{
+    public class VendorController : Controller
+    {
+        private readonly DBContext db = new DBContext();
+
+        public ActionResult OrderDetails()
+        {
+            var orders = db.OrderDetails.Include(e => e.UserDetail).Where(e => DateTime.Compare(e.OrderDate, DateTime.Today.Date) == 0);
+
+            return View(orders.ToList());
+        }
+    }
+}
